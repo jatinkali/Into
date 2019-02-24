1a50447727685117978fe5687bc39a3a456418cd
jatinkali <39433165+jatinkali@users.noreply.github.com>
2019-02-24 12:09:05 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,4 @@
+name = input("Enter your Name: ")
+age = input("Enter your age:\n ")
+result = ("Hello " + name + " I am Jatin this is my first python programme\n i got ur details\n name = " + name + "\n your age = " + age)
+print(result)
